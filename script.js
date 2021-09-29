38dc1c5ecb3265749c7c2e98fffcc248eab6e3fe
arjunkurup <arjunkurup24@gmail.com>
2021-09-29 03:06:23 -0700

    second update

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
 function openVideo(image,video){
 
     document.getElementById(image).style.display="none"
@@ -10,35 +28,37 @@ function openVideo(image,video){
 
 
 }
-// function openVideo2(){
-//     document.getElementById("image1").style.display="none"
-//     var video2=document.getElementById("vide1")
-//     video2.style.display="block"
-//     // video2.src+="autoplay=1"
+function openVideo2(){
+    document.getElementById("image1").style.display="none"
+    var video2=document.getElementById("vide1")
+    video2.style.display="block"
+    // video2.src+="autoplay=1"
 
 
-// }
-// function openVideo3(){
-//     document.getElementById("image2").style.display="none"
-//     var video3=document.getElementById("vide2")
-//     video3.style.display="block"
-//     // video3.src+="autoplay=1"
+}
+function openVideo3(){
+    document.getElementById("image2").style.display="none"
+    var video3=document.getElementById("vide2")
+    video3.style.display="block"
+    // video3.src+="autoplay=1"
 
-// }
+}
 
-// function openVideo4(){
-//     document.getElementById("image3").style.display="none"
-//     var video4=document.getElementById("vide3")
-//     video4.style.display="block"
-//     // video4.src+="autoplay=1"
-
-
-// }
-// function openVideo5(){
-//     document.getElementById("image4").style.display="none"
-//     var video5=document.getElementById("vide4")
-//     video5.style.display="block"
-//     // video5.src+="autoplay=1"
+function openVideo4(){
+    document.getElementById("image3").style.display="none"
+    var video4=document.getElementById("vide3")
+    video4.style.display="block"
+    // video4.src+="autoplay=1"
 
 
-// }
+}
+function openVideo5(){
+    document.getElementById("image4").style.display="none"
+    var video5=document.getElementById("vide4")
+    video5.style.display="block"
+    // video5.src+="autoplay=1"
+
+
+}
+//Get the button:
+
